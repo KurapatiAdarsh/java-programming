63a84f8056ef7c42caa3eda53a821d7fe4dcbfda
KurapatiAdarsh <161073800+KurapatiAdarsh@users.noreply.github.com>
2024-05-02 00:30:15 +0530

    Add files via upload

---
+++ guessthenumber.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Test {
+public static void main (String args[]){
+Scanner sc = new Scanner (System.in);
+Random o1 = new Random();
+int random =o1.nextInt(3);
+int choices=3;
+int cscore=0;
+int uscore=0;
+
+System.out.println("OP is generated a number between 1 to 20,match that number you have 3 choices");
+
+
+for (int i=1; i<=choices; i++){
+int userinput=sc.nextInt();
+
+if (userinput>random){
+System.out.println("you are number is too High");
+
+
+}
+
+
+else if (userinput<random) {
+System.out.println("your are number is too Low");
+}
+
+else {
+System.out.println("both are equal congrationals");
+uscore++;
+System.out.println("user score is "+uscore);
+break;
+}
+
+
+if (i==choices){
+System.out.println("sorry try again");
+cscore++;
+System.out.println("computer score is "+cscore);
+}
+}//i
+
+
+}
+}
